e4c7875ca66bfb89e30d90c7d351715b13ac1afe
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2024-05-13 15:52:15 +0700

    int main

--- construktor/construktor.cpp
+++ construktor/construktor.cpp
@@ -2,3 +2,47 @@
 #include <string>
 using namespace std;
 
+class Mahasiswa
+{
+public:
+	int nim;
+	string nama;
+
+	Mahasiswa()
+	{
+		nim = 0;
+		nama = " ";
+	};
+	Mahasiswa(int iNim)
+	{
+		nim = iNim;
+
+	};
+	Mahasiswa(string);
+	Mahasiswa(int iNim, string iNama);
+	void cetak()
+	{
+		cout << "NIM = " << nim << endl;
+		cout << "Nama = " << nama << endl;
+		cout << endl;
+	}
+
+};
+
+Mahasiswa::Mahasiswa(string iNama)
+{
+	nama = iNama;
+
+}
+
+Mahasiswa::Mahasiswa(int iNim, string iNama)
+{
+	nim = iNim;
+	nama = iNama;
+};
+
+int main()
+{
+
+}
+
